eadc6930b19df4f3e6cd64b04f27c7c0f9dbea74
Radu Cordunianu <82465411+Radush02@users.noreply.github.com>
2023-03-19 14:56:57 +0200

    Implementat heap sort, shell sort

--- main.py
+++ main.py
@@ -2,28 +2,59 @@ import time
 import random
 import sortari
 
-arr=[] #pt merge & radix
-arr_quick=[]
-for i in range(0,int(1e4)):
-    arr.append(random.randint(0,1000000))
-    arr_quick.append(arr[i])
+tests=[]
 
-start=time.time()
-sortari.mergeSort(arr)
-#print(*sortari.mergeSort(arr))
-print(f"Timp merge sort: {(time.time()-start)}")
+t=int(input("T = "))
+for i in range(0,t):
+    n=int(float(input("N = ")))
+    mx=int(float(input("Max = ")))
+    tests.append((n,mx))
 
-start=time.time()
-try:
-    sortari.quickSort(arr_quick, 0, len(arr) - 1)
-except RecursionError:
-    print("Lista este deja sortata (Quick sort).")
-else:
-    print(f"Timp quick sort: {time.time() - start}")
+for test in tests:
+    arr=[]
+    arr_de_sortat=[]
+    for i in range(0,int(test[0])):
+        arr.append(random.randint(0,test[1]))
+        arr_de_sortat.append(arr[i])
+    print(f"Pentru  N = {test[0]} Max = {test[1]}")
+    #Quick Sort
+    start=time.time()
+    try:
+        sortari.quickSort(arr_de_sortat, 0, len(arr) - 1)
+    except RecursionError:
+        print(f"Nu se poate sorta. Fie lista este deja sortata, fie s-au apelat prea multe apeluri recursive "
+              f"(Quick sort)")
+    else:
+        print(f"Timp quick sort: {time.time() - start}")
 
-start=time.time()
-sortari.radix_sort(arr)
-print(f"Timp radix sort: {time.time() - start}")
 
+    arr_de_sortat=[]
+    for i in range(0, int(test[0])): #folosim aceleasi numere
+        arr_de_sortat.append(arr[i])
+
+    # Merge sort
+    start=time.time()
+    sortari.mergeSort(arr_de_sortat)
+    print(f"Timp merge sort: {(time.time()-start)}")
+
+    #Radix sort
+    start=time.time()
+    sortari.radix_sort(arr_de_sortat) #Nu reconstruim lista intrucat merge sort nu este in-place
+    print(f"Timp radix sort: {time.time() - start}")
+
+
+    #Shell sort
+    arr_de_sortat=[]
+    for i in range(0, int(test[0])):
+        arr_de_sortat.append(arr[i])
+    start=time.time()
+    sortari.shellSort(arr_de_sortat)
+    print(f"Timp shell sort: {time.time() - start}")
+
+    #Heap sort
+    start=time.time()
+    sortari.heapSort(arr) #ultima sortare, deci putem folosi lista initiala
+    print(f"Timp heap sort: {time.time() - start}")
+    print("\n")
 
 
--- sortari.py
+++ sortari.py
@@ -58,7 +58,44 @@ def radix_sort(arr):
     for i in range(0,cif_max):
         counting(arr,i)
     return arr
-#Doar pentru concept. Nu va rula in programul principal.
+
+def shellSort(arr):
+    n=len(arr)
+    k=len(arr)//2
+    while k>0:
+        for i in range(k,n):
+            aux=arr[i]
+            j=i
+            while j>=k and arr[j-i]>aux:
+                arr[j]=arr[j-k]
+                j-=k
+            arr[j]=aux
+        k//=2
+
+def heapify(arr,i,n): #folosim max heap pt ordonare crescatoare
+    st=2*i+1
+    dr=2*i+2
+    mn=i
+
+    if st<n and arr[st] > arr[mn]: #arr[st] < arr[mn] pt minheap
+        mn=st
+    if dr<n and arr[dr] > arr[mn]: # ^^^
+        mn=dr
+
+    if mn!=i:
+        arr[i],arr[mn]=arr[mn],arr[i]
+        heapify(arr,mn,n)
+
+def heapSort(arr):
+    n=len(arr)
+    for i in range(n//2-1,-1,-1):
+        heapify(arr,i,n)
+    for i in range(n-1,0,-1):
+        arr[i],arr[0]=arr[0],arr[i]
+        heapify(arr,0,i)
+
+
+#Nu vor rula in programul principal datorita complexitatii. Bogo sort(O(n+1)!), bubble sort (O(n^2))
 def bogosort(arr):
     while bogo_sortat(arr)==0:
         shuffle(arr)
@@ -68,3 +105,10 @@ def bogo_sortat(arr):
         if arr[i]>arr[i+1]:
             return 0
     return 1
+
+def bubble_sort(arr):
+    n=len(arr)
+    for i in range(0,n):
+        for j in range(0,n-i-1):
+            if arr[j]>arr[j+1]:
+                arr[j],arr[j+1]=arr[j+1],arr[j]
